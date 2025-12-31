6378aff4f96689d91d64806ff08a462ce0df2a89
Shadman Shoumik <shadmanshoumik25@gmail.com>
2025-12-31 22:07:41 +0600

    add functionality to clear all stored meals, workouts, and total calories

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ class CalorieTracker {
     this._displayCaloriesBurned();
     this._displayCaloriesRemaining();
     this._displayCaloriesProgress();
+
+    document.getElementById('limit').value = this._calorieLimit;
   }
 
   // Public Methods/API //
@@ -66,6 +68,7 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+    Storage.clearAll();
     this._render();
   }
 
@@ -312,6 +315,14 @@ class Storage {
     });
 
     localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+
+  static clearAll() {
+    // localStorage.clear()
+
+    localStorage.removeItem('totalCalories');
+    localStorage.removeItem('meals');
+    localStorage.removeItem('workouts');
   }
 }
 
